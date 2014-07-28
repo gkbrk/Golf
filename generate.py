c6d56ee1a5dea820d8571e2fd08cb8531c07e2f7
Gökberk YALTIRAKLI <webdosusb@gmail.com>
2014-07-29 00:24:00 +0300

    Rewrote the code. (json configuration file support)
    It now supports custom compile.json files to configure the compilations.

--- generate.py
+++ generate.py
@@ -2,30 +2,44 @@ import template_engine
 import markdown
 import glob
 import re
+import json
+import sys
+import shutil
 
-def get_config():
-    config = {
-    'sitename': 'GKBRK',
-    'siteslogan': 'A Developers Website',
-    'copyright': 'Gokberk Yaltirakli'
-    }
-    return config
+if len(sys.argv) > 1:
+    compile_file = sys.argv[1]
+else:
+    compile_file = "compile.json"
 
-def generate_page(pagename):
-    page_filename = re.search("pages/(.*?).markdown", pagename).group(1)
-    page_contents = open(pagename).read().decode("utf-8").encode("ascii", "xmlcharrefreplace") #UTF-8 Character Support
-    page_title = re.search("#(.*?)#", page_contents).group(1)
-    
-    contents = markdown.markdown(page_contents)
-    values={}
-    values["contents"] = contents
-    values["title"] = page_title
-    values.update(get_config())
-    with open("template.html") as template_file:
-        generated_page = template_engine.apply_template(template_file.read(), values)
-    
-    with open("site/%s.html" % page_filename, "w+") as site_file:
-        site_file.write(generated_page)
+with open(compile_file) as compile_file_handle:
+    compile_options = json.loads(compile_file_handle.read())
 
-for page in glob.glob("pages/*.markdown"):
-    generate_page(page)
+for compile_folder in compile_options["compile_folders"]:
+    if compile_folder["method"] == "markdown-page":
+        for page in glob.glob(compile_folder["in"]):
+            page_filename = re.search(compile_folder["in"].replace("*", "(.*?)").replace("\\", "\\\\"), page).group(1)
+            with open(page) as page_file:
+                page_contents = page_file.read().decode("utf-8").encode("ascii", "xmlcharrefreplace") #UTF-8 Character Support
+            page_title = re.search("#(.*?)#", page_contents).group(1)
+
+            markdown_contents = markdown.markdown(page_contents)
+            values = {}
+            values["contents"] = markdown_contents
+            values["title"] = page_title
+            values.update(compile_options["page_variables"])
+
+            if "template_file" in compile_folder:
+                template_filename = compile_folder["template_file"]
+            else:
+                template_filename = compile_options["template_file"]
+            with open(template_filename) as template_file:
+                generated_page = template_engine.apply_template(template_file.read(), values)
+
+            with open(compile_folder["out"].replace("*", page_filename).replace("\\\\", "\\"), "w+") as html_file:
+                html_file.write(generated_page)
+    elif compile_folder["method"] == "static-file":
+        for static_file in glob.glob(compile_folder["in"]):
+            in_folder = compile_folder["in"] + "$"
+            file_name = re.search(in_folder.replace("*", "(.*?)").replace("\\", "\\\\"), static_file).group(1)
+            dest_name = compile_folder["out"].replace("*", file_name).replace("\\\\", "\\")
+            shutil.copy2(static_file, dest_name)
