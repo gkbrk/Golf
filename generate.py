616f999da579faa0a5c0a4b151b93b8273453afb
Gökberk YALTIRAKLI <webdosusb@gmail.com>
2014-07-29 13:30:49 +0300

    Started using os.path.normpath instead of dealing with the backslash madness at the json files.

--- generate.py
+++ generate.py
@@ -5,6 +5,7 @@ import re
 import json
 import sys
 import shutil
+import os
 
 if len(sys.argv) > 1:
     compile_file = sys.argv[1]
@@ -17,7 +18,8 @@ with open(compile_file) as compile_file_handle:
 for compile_folder in compile_options["compile_folders"]:
     if compile_folder["method"] == "markdown-page":
         for page in glob.glob(compile_folder["in"]):
-            page_filename = re.search(compile_folder["in"].replace("*", "(.*?)").replace("\\", "\\\\"), page).group(1)
+            page = os.path.normpath(page)
+            page_filename = re.search(os.path.normpath(compile_folder["in"]).replace("*", "(.*?)").replace("\\", "\\\\"), page).group(1)
             with open(page) as page_file:
                 page_contents = page_file.read().decode("utf-8").encode("ascii", "xmlcharrefreplace") #UTF-8 Character Support
             page_title = re.search("#(.*?)#", page_contents).group(1)
@@ -32,17 +34,17 @@ for compile_folder in compile_options["compile_folders"]:
                 values.update(compile_folder["page_variables"]) #Allow changing the variables based on the folder.
 
             if "template_file" in compile_folder:
-                template_filename = compile_folder["template_file"]
+                template_filename = os.path.normpath(compile_folder["template_file"])
             else:
-                template_filename = compile_options["template_file"]
+                template_filename = os.path.normpath(compile_options["template_file"])
             with open(template_filename) as template_file:
                 generated_page = template_engine.apply_template(template_file.read(), values)
 
-            with open(compile_folder["out"].replace("*", page_filename).replace("\\\\", "\\"), "w+") as html_file:
+            with open(os.path.normpath(compile_folder["out"].replace("*", page_filename)), "w+") as html_file:
                 html_file.write(generated_page)
     elif compile_folder["method"] == "static-file":
         for static_file in glob.glob(compile_folder["in"]):
-            in_folder = compile_folder["in"] + "$"
-            file_name = re.search(in_folder.replace("*", "(.*?)").replace("\\", "\\\\"), static_file).group(1)
-            dest_name = compile_folder["out"].replace("*", file_name).replace("\\\\", "\\")
+            static_file = os.path.normpath(static_file)
+            file_name = re.search(os.path.normpath(compile_folder["in"]).replace("*", "(.*?)").replace("\\", "\\\\") + "$", static_file).group(1)
+            dest_name = os.path.normpath(compile_folder["out"].replace("*", file_name))
             shutil.copy2(static_file, dest_name)
